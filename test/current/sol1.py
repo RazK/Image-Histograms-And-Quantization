673ce0ed8e7a24f86325aa9815b2ae35bb76e9ba
ROBO-LAPTOP\Raz <Raz@yosigal.com>
2017-11-04 17:14:51 +0200

    histogram_equaliztaion working correctly for RGB and for GREYSCALE

--- test/current/sol1.py
+++ test/current/sol1.py
@@ -3,17 +3,13 @@ import timeit
 import numpy as np
 from scipy.misc import imread as imread
 from scipy.interpolate import interp1d
-from skimage.color import rgb2gray, rgb2yiq as skimage_rgb2yiq, yiq2rgb as \
-    skimage_yiq2rgb
+from skimage.color import rgb2gray
 from matplotlib import pyplot as plt
 
 # Constants
 PIXEL_INTENSITY_MAX = 255
 PIXEL_INTENSITIES = PIXEL_INTENSITY_MAX + 1
-PIXEL_INTENSITIES_LIST = [i for i in range(0, PIXEL_INTENSITY_MAX + 1)]
-PIXEL_INTENSITIES_NORMALIZED = [i / PIXEL_INTENSITY_MAX for i in
-                                PIXEL_INTENSITIES_LIST]
-PIXEL_RANGE = (0, PIXEL_INTENSITY_MAX)
+PIXEL_RANGE = (0, PIXEL_INTENSITIES)
 PIXEL_RANGE_NORMALIZED = (0, 1)
 
 # Picture representation modes
@@ -35,11 +31,10 @@ def read_image(filename, representation):
     :param filename:        string containing the image filename to read.
     :param representation:  representation code, either 1 or 2 defining whether
                             the output should be a greyscale image (1) or an
-                            RGB
-                            image (2).
+                            RGB image (2).
     :return:                returns an image represented by a matrix of type
-    np.float64 with
-                            intensities normalized to the range [0,1]
+                            .float64 with intensities normalized to the
+                            range [0,1]
     """
     im = imread(filename)
     im_float = im.astype(np.float64)
@@ -49,7 +44,7 @@ def read_image(filename, representation):
 
 
 def display_image(image, cmap=None):
-    clipped = np.clip(image[:, :, :], 0, 1)
+    clipped = np.clip(image[..., :], 0, 1)
     plt.imshow(clipped, cmap=cmap)
     plt.show()
 
@@ -75,19 +70,65 @@ def imdisplay(filename, representation):
 def rgb2yiq(imRGB):
     """
     Transforms an RGB image into YIQ color space.
+    Remove alpha channel if exists.
     :param imRGB:   an RGB image.
     :return:        an YIQ image with the same dimensions as the input.
     """
-    return np.dot(imRGB, MATRIX_RGB2YIQ.T)
+    return np.dot(imRGB[:, :, :3], MATRIX_RGB2YIQ.T)
 
 
 def yiq2rgb(imYIQ):
     """
     Transforms an RGB image into YIQ color space.
+    Remove alpha channel if exists.
     :param imRGB:   an RGB image.
     :return:        an YIQ image with the same dimensions as the input.
     """
-    return np.dot(imYIQ, MATRIX_YIQ2RGB.T)
+    return np.dot(imYIQ[:, :, :3], MATRIX_YIQ2RGB.T)
+
+
+def intensity_equalize(intensities):
+    """
+    Performs histogram equalization of the given pixel intensities.
+    :param intensities: float64 image intensities with values in [0, 1].
+    :return:            list [im_eq, hist_orig, hist_eq] where:
+                        intensity_eq - is the equalized intensities. np.array
+                        with values in [0, 1].
+                        hist_orig - is a 256 bin histogram of the original
+                        intensities (array with shape (256,) ).
+                        hist_eq - is a 256 bin histogram of the
+                        equalized intensities (array with shape (256,) ).
+    """
+    # Translate [0,1] intensity range to [0,255] integer range
+    intensities = np.round(intensities * PIXEL_INTENSITY_MAX).astype(np.uint8)
+
+    # Build cumulative histogram of pixel intensities
+    hist_orig, bins = np.histogram(a=intensities, bins=PIXEL_INTENSITIES,
+                                   range=PIXEL_RANGE)
+    cdf = np.cumsum(hist_orig)
+
+    # Normalize cumulative histogram:
+    # C[k] = round(((C[k] / NUM_OF_PIXELS) * PIXEL_INTENSITY_MAX))
+    cdf_n = (cdf * PIXEL_INTENSITY_MAX / cdf[-1]).astype(np.uint8)
+
+    # Map original intensity values to their equalized values
+    intensity_eq = np.array(list(map(lambda i: cdf_n[i], intensities))).astype(
+        np.uint8)
+
+    # Calculate histogram of equalized intensity values
+    hist_eq, bins = np.histogram(intensity_eq, PIXEL_INTENSITIES)
+
+    return (intensity_eq / PIXEL_INTENSITY_MAX), hist_orig, hist_eq
+
+
+def is_greyscale(image):
+    """
+    Returns true if image is greyscale, otherwise false.
+    :param image:   an image to check
+    :return:        true if image is greyscale, otherwise false
+    """
+    # Image is just (width * height) with no pixel channels
+    return (len(image.shape) == 2)
 
 
 def histogram_equalize(im_orig):
@@ -102,32 +143,26 @@ def histogram_equalize(im_orig):
                     hist_eq - is a 256 bin histogram of the equalized image
                     (array with shape (256,) ).
     """
-    # Convert image to YIQ for equalizing Y values
-    yiq = skimage_rgb2yiq(im_orig) * PIXEL_INTENSITY_MAX
-    y = yiq[..., INDEX_Y].astype(np.uint8)
+    # Determine if RGB or GREYSCALE
+    if (is_greyscale(im_orig)):
+        intensities_eq, hist_orig, hist_eq = intensity_equalize(im_orig)
+        im_eq = intensities_eq
+    else:
+        # Convert image RGB [0-1] to YIQ [0-255] for equalizing Y values
+        yiq = rgb2yiq(im_orig)
+        y_eq, hist_orig, hist_eq = intensity_equalize(yiq[..., INDEX_Y])
 
-    # Build cumulative histogram of Y values
-    hist_orig, bins = np.histogram(y, 256, (0, 255))
-    cdf = np.cumsum(hist_orig)
-    cdf_n = (cdf * PIXEL_INTENSITY_MAX / cdf[-1]).astype(np.uint8)
+        # Update Y with equalized values
+        yiq[..., INDEX_Y] = y_eq
 
-    # Map original y values to their equalized values
-    y_eq = np.array(list(map(lambda y: cdf_n[y], y))).astype(np.uint8)
-
-    # Calculate histogram of equalized Y values
-    hist_eq, bins = np.histogram(y_eq, 256)
-
-    # Update Y with equalized values
-    yiq[..., INDEX_Y] = y_eq
-
-    # Back to RGB
-    im_eq = skimage_yiq2rgb(yiq / PIXEL_INTENSITY_MAX)
+        # Back to RGB
+        im_eq = yiq2rgb(yiq)
 
     # Done!
     return [im_eq, hist_orig, hist_eq]
 
 
 TEST_IMAGE = "external\\Low Contrast.jpg"
-im = read_image(TEST_IMAGE, MODE_RGB)
+im = read_image(TEST_IMAGE, MODE_GRAYSCALE)
 im_eq = histogram_equalize(im)[0]
-display_image(im_eq)
+display_image(im_eq, plt.cm.gray)
